56c6c6e7649d89c93a295811c31b492941227946
AnanshaG <40948880+AnanshaG@users.noreply.github.com>
2026-07-09 23:37:52 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+
+        int[] res = IntStream.concat(Arrays.stream(nums), Arrays.stream(nums)).toArray();
+        return res;
+        
+    }
+}
