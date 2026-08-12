96fad8ad3ffa5bfae15eaacb2579a1600d8f873f
AnanshaG <40948880+AnanshaG@users.noreply.github.com>
2026-08-12 23:21:47 +0530

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int max = Arrays.stream(piles).max().getAsInt();
+        int left = 1;
+        int right = max;
+        int res = max;
+        while(left <= right){
+            int mid = (left + right)/2;
+            int hrs = 0;
+            for(int val: piles){
+                hrs += (val + mid - 1)/mid;
+            }
+            //System.out.println(hrs + " : " + mid);
+            if(hrs > h){
+                left = mid+1;
+            }
+            else if(hrs <= h){
+                res = mid;
+                right = mid - 1;
+            }
+            
+            
+
+        }
+
+        return res;
+        
+    }
+}
